19feca79d7a136b74292f06270143802185cd2e6
smilingkw-glitch <smiling.kw@gmail.com>
2025-12-11 18:55:45 +0530

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,7 @@
+export function footer() {
+    return `
+        <footer style="padding:10px; background:#222; color:white; margin-top:20px; text-align:center;">
+            <p>© 2025 My Modular JS App</p>
+        </footer>
+    `;
+}
